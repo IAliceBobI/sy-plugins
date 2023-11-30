03712a4acd8b9d669f6e6da115cea3ea277ea554
008 <007>
2023-12-01 02:01:12 +0800

    strong

--- sy-tomato-plugin/src/BackLink.svelte
+++ sy-tomato-plugin/src/BackLink.svelte
@@ -26,7 +26,6 @@
                         eventType == EventType.loaded_protyle_dynamic ||
                         eventType == EventType.loaded_protyle_static
                     ) {
-                        console.log(eventType);
                         title =
                             detail?.protyle?.title?.editElement?.textContent?.trim() ??
                             "";
@@ -47,7 +46,7 @@
         for (const d of bls.backlinks) {
             const bdocs = await siyuan.getBacklinkDoc(docID, d.id);
             for (const doc of bdocs.backlinks) {
-                for (const p of doc.blockPaths) {
+                for (const p of doc?.blockPaths ?? []) {
                     if (p.type == "NodeDocument") continue;
                     const { content } = await siyuan.getBlockMarkdownAndContent(
                         p.id,
@@ -59,7 +58,7 @@
         for (const d of bls.backmentions) {
             const bmdocs = await siyuan.getBackmentionDoc(docID, d.id);
             for (const doc of bmdocs.backmentions) {
-                for (const p of doc.blockPaths) {
+                for (const p of doc?.blockPaths ?? []) {
                     if (p.type == "NodeDocument") continue;
                     const content = keepContext(p.name, title, 8);
                     mentionlinks = [...mentionlinks, { id: p.id, content }];
@@ -167,6 +166,6 @@
         background: #e3d8d8;
         border-radius: 4px;
         padding: 2px 8px;
-        font-size: x-large;
+        font-size: large;
     }
 </style>
