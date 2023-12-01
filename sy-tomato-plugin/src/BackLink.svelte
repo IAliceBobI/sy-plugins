41cd5910ed55dac0f7c1bb5f9eb343dc2dbf83a6
008 <007>
2023-12-02 01:29:43 +0800

    auto

--- sy-tomato-plugin/src/BackLink.svelte
+++ sy-tomato-plugin/src/BackLink.svelte
@@ -199,12 +199,14 @@
 <style>
     .reftext-small {
         background: var(--b3-theme-surface);
+        color: var(--b3-theme-on-surface);
         border-radius: 4px;
         padding: 2px 8px;
         font-size: small;
     }
     .reftext {
         background: var(--b3-theme-surface);
+        color: var(--b3-theme-on-surface);
         border-radius: 4px;
         padding: 2px 8px;
         font-size: large;
