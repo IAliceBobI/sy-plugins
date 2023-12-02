7e3b184f54ea40fafc3da06f444c8b0f079ede5a
008 <007>
2023-12-03 01:16:12 +0800

    rm add

--- sy-tomato-plugin/src/BackLinkBottomBox.ts
+++ sy-tomato-plugin/src/BackLinkBottomBox.ts
@@ -17,6 +17,7 @@ class BackLinkBottomBox {
                     const docID = detail?.protyle?.block.rootID ?? "";
                     if (docID) {
                         const lastID = await this.getLastBlockID(docID);
+                        await this.rmbacklink(docID);
                         await this.getBackLinks(docID, lastID);
                     }
                 },
@@ -24,40 +25,66 @@ class BackLinkBottomBox {
         });
     }
 
+    async rmbacklink(docID: string) {
+        const rows = await siyuan.sql(`select id from blocks where ial like '%${TOMATOBACKLINKKEY}%' and root_id="${docID}"`)
+        console.log(rows)
+        for (const row of rows) {
+            await siyuan.safeDeleteBlock(row['id'])
+        }
+    }
+
     async getLastBlockID(docID: string) {
-        const ids = await siyuan.getChildBlocks(docID);
-        const len = ids.length;
-        if (len > 0) return ids[len - 1].id;
-        return "";
+        const idtypes = await siyuan.getChildBlocks(docID);
+        idtypes.reverse();
+        for (const idtype of idtypes) {
+            const row = await siyuan.sqlOne(`select ial from blocks where id="${idtype.id}"`)
+            const ial: string = row?.ial ?? "";
+            if (!ial.includes(TOMATOBACKLINKKEY)) {
+                return idtype.id
+            }
+        }
+        return ''
     }
 
     async getBackLinks(docID: string, lastID: string) {
         const backlink2 = await siyuan.getBacklink2(docID);
-        for (const backlink of backlink2.backlinks) {
-            const backlinkDoc = await siyuan.getBacklinkDoc(
-                docID,
-                backlink.id,
-            );
-            for (const bkPath of backlinkDoc.backlinks) {
-                const div = document.createElement("div") as HTMLDivElement;
-                div.innerHTML = bkPath.dom;
-                let blockID = div.firstElementChild.getAttribute(DATA_NODE_ID);
-                const data_type = div.firstElementChild.getAttribute(DATA_TYPE);
-                if (data_type == 'NodeListItem') {
-                    const [listID] = await siyuan.findListType(blockID);
-                    if (listID) {
-                        blockID = listID;
-                    }
-                }
-                let md = `{{ select * from blocks where id = "${blockID}"}}`;
-                md += "\n"
-                md += `{: ${TOMATOBACKLINKKEY}="1"}`
-                if (lastID) {
-                    await siyuan.insertBlockAfter(md, lastID);
-                } else {
-                    await siyuan.insertBlockAsChildOf(md, lastID);
-                }
+        for (const memtion of backlink2.backmentions.reverse()) {
+            const memtionDoc = await siyuan.getBackmentionDoc(docID, memtion.id);
+            for (const bkPath of memtionDoc.backmentions) {
+                await this.embedDom(bkPath, lastID);
             }
+        }
+        for (const backlink of backlink2.backlinks) {
+            const backlinkDoc = await siyuan.getBacklinkDoc(docID, backlink.id);
+            for (const bkPath of backlinkDoc.backlinks.reverse()) {
+                await this.embedDom(bkPath, lastID);
+            }
+        }
+        await this.insertMd("---", lastID);
+    }
+
+    private async embedDom(bkPath: Backlink, lastID: string) {
+        const div = document.createElement("div") as HTMLDivElement;
+        div.innerHTML = bkPath.dom;
+        let blockID = div.firstElementChild.getAttribute(DATA_NODE_ID);
+        const data_type = div.firstElementChild.getAttribute(DATA_TYPE);
+        if (data_type == 'NodeListItem') {
+            const [listID] = await siyuan.findListType(blockID);
+            if (listID) {
+                blockID = listID;
+            }
+        }
+        const md = `{{ select * from blocks where id = "${blockID}"}}`;
+        await this.insertMd(md, lastID);
+    }
+
+    private async insertMd(md: string, lastID: string) {
+        md += "\n";
+        md += `{: ${TOMATOBACKLINKKEY}="1"}`;
+        if (lastID) {
+            await siyuan.insertBlockAfter(md, lastID);
+        } else {
+            await siyuan.insertBlockAsChildOf(md, lastID);
         }
     }
 }
