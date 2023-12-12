77dd2a7ab8c3a1533f5189137e5a3b038c44beed
008 <007>
2023-12-12 15:36:44 +0800

    dedup

--- sy-tomato-plugin/src/LinkBox.ts
+++ sy-tomato-plugin/src/LinkBox.ts
@@ -1,8 +1,12 @@
-import { Lute, Plugin } from "siyuan";
+import { IProtyle, Lute, Plugin } from "siyuan";
 import { EventType, events } from "@/libs/Events";
 import * as gconst from "@/libs/gconst";
 import { siyuan } from "@/libs/utils";
 import * as utils from "@/libs/utils";
+
+function getDocNameFromProtyle(protyle?: IProtyle) {
+    return protyle?.title?.editElement?.textContent ?? "";
+}
 
 class LinkBox {
     private plugin: Plugin;
@@ -18,8 +22,9 @@ class LinkBox {
             editorCallback: async (protyle) => {
                 const ids = this.getSelectedIDs(protyle);
                 if (ids.length == 0) ids.push(events.lastBlockID);
+                const docName = getDocNameFromProtyle(protyle);
                 for (const id of ids)
-                    await this.addLink(id, protyle?.wysiwyg?.element);
+                    await this.addLink(id, docName, protyle?.wysiwyg?.element);
             },
         });
         this.plugin.eventBus.on(EventType.open_menu_content, async ({ detail }) => {
@@ -30,7 +35,7 @@ class LinkBox {
                 accelerator: "⌥/",
                 click: () => {
                     const blockID = detail?.element?.getAttribute("data-node-id") ?? "";
-                    if (blockID) this.addLink(blockID, detail?.protyle?.wysiwyg?.element);
+                    if (blockID) this.addLink(blockID, getDocNameFromProtyle(detail?.protyle), detail?.protyle?.wysiwyg?.element);
                 },
             });
         });
@@ -42,26 +47,29 @@ class LinkBox {
             iconHTML: "",
             label: this.plugin.i18n.bilink,
             click: () => {
+                const docName = getDocNameFromProtyle(detail?.protyle);
                 for (const element of detail.blockElements) {
                     const blockID = utils.getID(element);
                     if (blockID) {
-                        this.addLink(blockID, detail?.protyle?.wysiwyg?.element);
+                        this.addLink(blockID, docName, detail?.protyle?.wysiwyg?.element);
                     }
                 }
             }
         });
     }
 
-    private async turn2static(blockID: string, md: string, links: string[], element: HTMLLIElement) {
-        let mdTemp = md;
-        for (const lnk of links) {
-            if (lnk.includes("'")) {
-                const st = lnk.replace(/'/g, '"');
-                mdTemp = mdTemp.replace(lnk, st);
+    private async turn2static(blockID: string, dom: HTMLElement, element: HTMLLIElement) {
+        let changed = false;
+        for (const e of dom.querySelectorAll("[data-type*=\"block-ref\"]")) {
+            const type = e.getAttribute("data-subtype");
+            if (type == "d") {
+                e.setAttribute("data-subtype", "s");
+                changed = true;
             }
         }
-        if (mdTemp != md) {
-            await siyuan.safeUpdateBlock(blockID, mdTemp);
+        if (changed) {
+            const md = this.lute.BlockDOM2Md(dom.innerHTML);
+            await siyuan.safeUpdateBlock(blockID, md);
             const e = element.querySelector(`[${gconst.DATA_NODE_ID}="${blockID}"]`) as HTMLElement;
             if (e) {
                 document.getSelection().collapse(e, 1);
@@ -73,40 +81,30 @@ class LinkBox {
         const { dom } = await siyuan.getBlockDOM(blockID);
         const div = document.createElement("div") as HTMLElement;
         div.innerHTML = dom;
-        const ids = [];
-        const links = [];
+        const ids: Set<string> = new Set();
         for (const e of div.querySelectorAll("[data-type*=\"block-ref\"]")) {
             const id = e.getAttribute(gconst.DATA_ID);
-            const txt = e.textContent;
-            const type = e.getAttribute("data-subtype");
-            ids.push(id);
-            if (type == "d") {
-                links.push(`((${id} '${txt}'))`);
-            } else {
-                links.push(`((${id} "${txt}"))`);
-            }
+            ids.add(id);
         }
-        return { links, ids, md: this.lute.BlockDOM2Md(dom).trim(), content: div.innerText };
+        return { ids, dom: div };
     }
 
-    private async addLink(blockID: string, element: HTMLLIElement) {
-        const { links, ids, md, content } = await this.extractLinksFromDom(blockID);
-        if (ids.length <= 0) return;
-        const docID = await siyuan.getDocIDByBlockID(blockID);
-        if (!docID) return;
-        const { content: docName } = await siyuan.getBlockMarkdownAndContent(docID);
-        if (!docName) return;
-        await this.turn2static(blockID, md, links, element);
-        for (const link of ids) {
-            const row = await siyuan.sqlOne(`select type from blocks where id="${link}"`);
+    private async addLink(blockID: string, docName: string, element: HTMLLIElement) {
+        const { ids, dom } = await this.extractLinksFromDom(blockID);
+        if (ids.size <= 0) return;
+        await this.turn2static(blockID, dom, element);
+        const idRows = utils.chunks(await Promise.all(Array.from(ids.keys()).map((id) => {
+            return [id, siyuan.sqlOne(`select type from blocks where id="${id}"`)];
+        }).flat()), 2) as [string, Block][];
+        for (const [id, row] of idRows) {
             const idType = row?.type ?? "";
             if (!idType) continue;
             if (idType == "d") {
-                const backLink = `((${blockID} '${content}'))`;
-                await siyuan.insertBlockAsChildOf(backLink, link);
+                const backLink = `((${blockID} '${dom.innerText}'))`;
+                await siyuan.insertBlockAsChildOf(backLink, id);
             } else {
                 const backLink = `((${blockID} "[${docName}]"))`;
-                const { dom } = await siyuan.getBlockDOM(link);
+                const { dom } = await siyuan.getBlockDOM(id);
                 const md = this.lute.BlockDOM2Md(dom).trim();
                 if (md.includes(backLink)) continue;
                 const parts = md.split("\n");
@@ -114,10 +112,10 @@ class LinkBox {
                     const lastLine = parts[parts.length - 2];
                     parts[parts.length - 2] = lastLine + backLink;
                 }
-                await siyuan.safeUpdateBlock(link, parts.join("\n"));
+                await siyuan.safeUpdateBlock(id, parts.join("\n"));
             }
         }
-        await siyuan.pushMsg(`已经插入${ids.length}个链接。`);
+        await siyuan.pushMsg(`已经插入${ids.size}个链接。`);
     }
 
     private getSelectedIDs(protyle: any) {
