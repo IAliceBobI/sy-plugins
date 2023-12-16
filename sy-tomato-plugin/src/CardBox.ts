169eaf7b21338b469d249954422c3cbf217a6557
008 <007>
2023-12-16 17:59:18 +0800

    clean card

--- sy-tomato-plugin/src/CardBox.ts
+++ sy-tomato-plugin/src/CardBox.ts
@@ -1,4 +1,4 @@
-import { Plugin } from "siyuan";
+import { Plugin, confirm } from "siyuan";
 import { getID, siyuan, sleep } from "@/libs/utils";
 import "./index.scss";
 import { events } from "@/libs/Events";
@@ -12,12 +12,14 @@ class CardBox {
             langKey: "removeBrokenCards",
             hotkey: "",
             globalCallback: async () => {
-                const ids = await siyuan.removeBrokenCards();
-                if (ids.length) {
-                    siyuan.pushMsg(`${this.plugin.i18n.removedBrokenCards}${ids}`);
-                } else {
-                    siyuan.pushMsg(this.plugin.i18n.thereIsNoInvalidCards);
-                }
+                confirm(this.plugin.i18n.removeBrokenCards, `<strong>务必，请先看说明，或者备份好。</strong><a href="https://gitee.com/TokenzQdBN/sy-plugins/blob/main/sy-tomato-plugin/README_zh_CN.md#%E6%B8%85%E7%90%86%E5%A4%B1%E6%95%88%E9%97%AA%E5%8D%A1">说明</a>`, async () => {
+                    const ids = await siyuan.removeBrokenCards();
+                    if (ids.length) {
+                        siyuan.pushMsg(`${this.plugin.i18n.removedBrokenCards}${ids}`);
+                    } else {
+                        siyuan.pushMsg(this.plugin.i18n.thereIsNoInvalidCards);
+                    }
+                })
             },
         });
         this.plugin.addCommand({
