806cdaf94c088b859cd9280e428cea22cc4836d1
008 <007>
2023-12-31 01:49:42 +0800

    move block

--- sy-progressive-plugin/src/PieceMovingBox.ts
+++ sy-progressive-plugin/src/PieceMovingBox.ts
@@ -1,9 +1,12 @@
-import { IProtyle, Plugin } from "siyuan";
-import { isProtylePiece } from "./helper";
+import { IProtyle, Lute, Plugin } from "siyuan";
+import { getDocIalMark, isProtylePiece } from "./helper";
+import { NewLute, cleanDiv, getBlockDiv, isValidNumber, siyuan } from "../../sy-tomato-plugin/src/libs/utils";
+import { DATA_NODE_ID, MarkKey, PROTYLE_WYSIWYG_SELECT } from "../../sy-tomato-plugin/src/libs/gconst";
 
 class PieceMovingBox {
     private plugin: Plugin;
-    private settings: SettingCfgType;
+    settings: SettingCfgType;
+    private lute: Lute;
 
     blockIconEvent(detail: any) {
         if (!this.plugin) return;
@@ -29,6 +32,7 @@ class PieceMovingBox {
     async onload(plugin: Plugin, settings: SettingCfgType) {
         this.plugin = plugin;
         this.settings = settings;
+        this.lute = NewLute();
         this.plugin.eventBus.on("open-menu-content", async ({ detail }) => {
             const protyle: IProtyle = detail.protyle;
             if (isProtylePiece(protyle)) {
@@ -58,11 +62,35 @@ class PieceMovingBox {
     }
 
     private async moveBlock(blockID: string, delta: number) {
-
+        if (delta == 0) return;
+        const row = await siyuan.getDocRowByBlockID(blockID);
+        const attrs = await siyuan.getBlockAttrs(row.id);
+        const bookID = attrs[MarkKey]?.split("#")[1]?.split(",")[0];
+        let pieceNum = Number(attrs[MarkKey]?.split("#")[1]?.split(",")[1]);
+        if (isValidNumber(pieceNum) && bookID) {
+            pieceNum += delta;
+            if (pieceNum >= 0) {
+                const row = await siyuan.sqlOne(`select id from blocks where type='d' and ial like "%${getDocIalMark(bookID, pieceNum)}%"`);
+                if (row?.id) {
+                    const div = await getBlockDiv(blockID);
+                    cleanDiv(div, false);
+                    const md = this.lute.BlockDOM2Md(div.outerHTML);
+                    if (delta < 0) {
+                        await siyuan.appendBlock(md, row.id);
+                    } else {
+                        await siyuan.insertBlockAsChildOf(md, row.id);
+                    }
+                    await siyuan.safeUpdateBlock(blockID, "");
+                }
+            }
+        }
     }
 
     private async move(protyle: IProtyle, delta: number) {
-
+        const nodes = protyle.element.querySelectorAll(`.${PROTYLE_WYSIWYG_SELECT}`);
+        for (const e of nodes) {
+            await this.moveBlock(e.getAttribute(DATA_NODE_ID), delta);
+        }
     }
 }
 
