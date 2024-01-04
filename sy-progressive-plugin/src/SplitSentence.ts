2b1a226e1913c15b83458800a17613ab6840b3f6
008 <007>
2024-01-04 12:16:44 +0800

    content split

--- sy-progressive-plugin/src/SplitSentence.ts
+++ sy-progressive-plugin/src/SplitSentence.ts
@@ -3,18 +3,32 @@ import { siyuan } from "../../sy-tomato-plugin/src/libs/utils";
 
 export class SplitSentence {
     private noteID: string;
+    private lastID: string;
+
     constructor(noteID: string) {
         this.noteID = noteID;
     }
+
     async split() {
         const rows = (await Promise.all((await siyuan.getChildBlocks(this.noteID))
+            .filter(i => i.type == "p")
             .map(b => siyuan.sqlOne(`select id,content,ial from blocks 
             where id="${b.id}"
-            and (type = "p" or type = "l")
             and content != "" and content is not null
             and ial like "%${RefIDKey}=%"`)))).filter(i => i.content);
         for (const row of rows) {
-            console.log(row.ial)
+            this.lastID = row.id;
+            const ref = getIDFromIAL(row.ial)
+            if (ref) {
+                row.content
+            }
         }
     }
+}
+
+function getIDFromIAL(ial: string) {
+    // {: updated="20240104110156" custom-progref="20240103165224-jdum4t6" id="20240104110156-8tsr201"}
+    const ref = ial.match(/custom-progref="([^"]+)"/);
+    if (ref) return ref[1] ?? ""
+    return "";
 }
