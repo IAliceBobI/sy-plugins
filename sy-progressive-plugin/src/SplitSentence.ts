c126998cf687f318c668be9053cbfb47b093dbd0
008 <007>
2024-01-07 10:45:09 +0800

    auto

--- sy-progressive-plugin/src/SplitSentence.ts
+++ sy-progressive-plugin/src/SplitSentence.ts
@@ -25,7 +25,6 @@ export class SplitSentence {
                         await siyuan.insertBlockAfter("* " + b.blocks.join("\n* ") + `\n{: ${RefIDKey}="${b.ref}"}`, this.lastID);
                     }
                 }
-                await siyuan.insertBlockAfter("", this.lastID);
             }
         });
     }
