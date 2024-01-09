8d435393537a89fb210e4ce54e3b228a27e71100
008 <007>
2024-01-09 20:52:01 +0800

    deps

--- scripts/clean.zsh
+++ scripts/clean.zsh
@@ -6,8 +6,10 @@ cd ${dir}
 cd "${dir}/sy-progressive-plugin"
 rm -rf dev dist .eslintcache package.zip node_modules
 pnpm i
+pnpm update
 
 cd "${dir}/sy-tomato-plugin"
 rm -rf dev dist .eslintcache package.zip node_modules
 pnpm i
+pnpm update
 
