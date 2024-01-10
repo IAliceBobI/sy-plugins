4c4bf027639e3d2cafd8f7765c26e9d730c6a162
008 <007>
2024-01-10 09:34:38 +0800

    auto

--- sy-progressive-plugin/src/helper.ts
+++ sy-progressive-plugin/src/helper.ts
@@ -219,9 +219,9 @@ export function splitByBlockCount(groups: WordCountType[][], blockNumber: number
 
 export async function copyBlock(id: string, lute: Lute, mark?: string) {
     const { div: tempDiv } = await utils.getBlockDiv(id);
+    utils.cleanDiv(tempDiv, true);
     tempDiv.setAttribute(RefIDKey, id);
     if (mark) tempDiv.setAttribute(mark, "1");
-    utils.cleanDiv(tempDiv, true);
     let md = lute.BlockDOM2Md(tempDiv.outerHTML);
     return md.trim();
 }
