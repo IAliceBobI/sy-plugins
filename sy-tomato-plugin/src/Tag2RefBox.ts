11375c99cf81c7cc8fe6420466fee38e64b99abf
008 <007>
2024-01-13 15:41:45 +0800

    tag 2 ref start

--- sy-tomato-plugin/src/Tag2RefBox.ts
+++ sy-tomato-plugin/src/Tag2RefBox.ts
@@ -8,18 +8,14 @@ class Tag2RefBox {
     async onload(plugin: Plugin) {
         this.plugin = plugin;
         this.settingCfg = (plugin as any).settingCfg;
-        events.addListener("BackLinkBottomBox", (eventType, detail) => {
+        events.addListener("Tomato-Tag2RefBox", (eventType, detail) => {
             if (eventType == EventType.loaded_protyle_static) {
-                navigator.locks.request("BackLinkBottomBoxLock", { ifAvailable: true }, async (lock) => {
+                navigator.locks.request("Tomato-Tag2RefBox-Lock", { ifAvailable: true }, async (lock) => {
                     if (lock) {
                         const protyle = detail.protyle as IProtyle;
-                        if (!protyle || !protyle.element) return;
-                        if (protyle.element.classList.contains("card__block")) return;
-                        const item = protyle.wysiwyg?.element;
-                        if (!item) return;
                         const nextDocID = protyle.block.rootID ?? "";
                         if (!nextDocID) return;
-
+                        console.log("xxx")
                     }
                 });
             }
