18ee468e2d3ae8595853560f2b1a6ca77fe2dbc5
008 <007>
2024-01-20 20:45:49 +0800

    click

--- sy-tomato-plugin/src/CardPriorityBox.ts
+++ sy-tomato-plugin/src/CardPriorityBox.ts
@@ -13,8 +13,11 @@ class CardPriorityBox {
         this.plugin.addCommand({
             langKey: "cardPrioritySet",
             hotkey: "F6",
-            editorCallback: (protyle: IProtyle) => {
-                this.updateDocPriorityLock(protyle);
+            editorCallback: async (protyle: IProtyle) => {
+                const docID = protyle?.block?.rootID;
+                if (!docID) return;
+                const blocks = await siyuan.getTreeRiffCardsAll(docID);
+                this.updateDocPriorityBatchDialog(blocks);
             },
         });
 
@@ -23,14 +26,17 @@ class CardPriorityBox {
             menu.addItem({
                 label: this.plugin.i18n.cardPrioritySet,
                 accelerator: "F6",
-                click: () => {
-                    this.updateDocPriorityLock(detail.protyle);
+                click: async () => {
+                    const docID = detail?.protyle?.block?.rootID;
+                    if (!docID) return;
+                    const blocks = await siyuan.getTreeRiffCardsAll(docID);
+                    this.updateDocPriorityBatchDialog(blocks);
                 },
             });
             menu.addItem({
                 label: "为闪卡设置优先级",
                 click: () => {
-                    this.updatePriority(detail.protyle, [detail.element]);
+                    this.updatePrioritySelected([detail.element]);
                 },
             });
         });
@@ -42,7 +48,7 @@ class CardPriorityBox {
                     if (!protyle) return;
                     const element = protyle?.wysiwyg?.element as HTMLElement;
                     if (lock && element) {
-                        await addBtns(element);
+                        await this.addBtns(element);
                     }
                 });
             }
@@ -54,46 +60,49 @@ class CardPriorityBox {
         detail.menu.addItem({
             label: "为闪卡设置优先级",
             click: () => {
-                this.updatePriority(detail.protyle, detail.blockElements);
+                this.updatePrioritySelected(detail.blockElements);
             }
         });
     }
 
-    private async updatePriority(protyle: IProtyle, elements: HTMLElement[]) {
+    private async updatePrioritySelected(elements: HTMLElement[], priority?: number) {
         const blocks = (await Promise.all(elements.map(div => {
             return getID(div, [CUSTOM_RIFF_DECKS]);
         }).filter(i => !!i).map(id => siyuan.getBlockAttrs(id)))).map(ial => {
             return { ial };
         }).filter(b => !!b.ial[CUSTOM_RIFF_DECKS]);
-        return this.updateDocPriorityLock(protyle, blocks as any);
+        return this.updateDocPriorityBatchDialog(blocks as any, priority);
     }
 
-    private async updateDocPriorityLock(protyle: IProtyle, blocks?: Block[]) {
-        new DialogText("输入新的优先级", "50", async (newPriority: string) => {
-            const p = Number(newPriority);
-            if (isValidNumber(p)) {
-                return navigator.locks.request("CardPriorityBox.updateDocPriorityLock", { ifAvailable: true }, async (lock) => {
-                    if (lock) {
-                        await siyuan.pushMsg(`设置闪卡优先级为：${newPriority}`);
-                        const count = await this.updateDocPriority(protyle, p, blocks);
-                        await siyuan.pushMsg(`已经调整了${count}个闪卡的优先级`);
-                    } else {
-                        await siyuan.pushMsg("正在修改优先级，请耐心等候……");
-                    }
-                });
+    private async updateDocPriorityBatchDialog(blocks: Block[], priority?: number) {
+        if (isValidNumber(priority)) {
+            await this.updateDocPriorityLock(priority, blocks);
+        } else {
+            new DialogText("输入新的优先级", "50", async (priorityTxt: string) => {
+                const priority = Number(priorityTxt);
+                if (isValidNumber(priority)) {
+                    return this.updateDocPriorityLock(priority, blocks);
+                } else {
+                    await siyuan.pushMsg(`您的输入有误：${priorityTxt}`);
+                }
+            });
+        }
+    }
+
+    private updateDocPriorityLock(newPriority: number, blocks: Block[]): any {
+        return navigator.locks.request("CardPriorityBox.updateDocPriorityLock", { ifAvailable: true }, async (lock) => {
+            if (lock) {
+                await siyuan.pushMsg(`设置闪卡优先级为：${newPriority}`, 2000);
+                const count = await this.updateDocPriority(newPriority, blocks);
+                await siyuan.pushMsg(`已经调整了${count}个闪卡的优先级`, 2000);
             } else {
-                await siyuan.pushMsg(`您的输入有误：${newPriority}`);
+                await siyuan.pushMsg("正在修改优先级，请耐心等候……", 2000);
             }
         });
     }
 
-    private async updateDocPriority(protyle: IProtyle, newPriority: number, blocks?: Block[]) {
+    private async updateDocPriority(newPriority: number, blocks: Block[]) {
         newPriority = ensureValidPriority(newPriority);
-        if (!blocks) {
-            const docID = protyle?.block?.rootID;
-            if (!docID) return;
-            blocks = await siyuan.getTreeRiffCardsAll(docID);
-        }
         const tasks = await Promise.all(blocks.map(block => {
             const ial = block.ial as unknown as AttrType;
             const priority = readPriority(ial);
@@ -117,43 +126,69 @@ class CardPriorityBox {
         options.cards.sort((a, b) => attrMap.get(b.blockID) - attrMap.get(a.blockID));
         return options;
     }
+
+    private async addBtns(element: HTMLElement) {
+        [...element.querySelectorAll(`[${CUSTOM_RIFF_DECKS}]`)]
+            .filter(e => {
+                const tomato = [...e.querySelectorAll(`[${TOMATO_CONTROL_ELEMENT}]`)];
+                if (tomato.length == 0) return true;
+                tomato.slice(1).forEach(e => e.parentElement.removeChild(e));
+                return tomato.reduce((ret, te: HTMLElement) => {
+                    if (te.childElementCount == 0) {
+                        te.parentElement.removeChild(te);
+                        return true;
+                    }
+                    return ret;
+                }, false);
+            })
+            .map((e: HTMLElement) => {
+                let priority = Number(e.getAttribute(CARD_PRIORITY) ?? "50");
+                if (!isValidNumber(priority)) priority = 50;
+
+                const div = e.appendChild(document.createElement("div"));
+                div.setAttribute(TOMATO_CONTROL_ELEMENT, "1");
+                // div.style.paddingLeft = "20px";
+                div.contentEditable = "false";
+                div.style.display = "flex"
+                div.style.justifyContent = "space-between"
+                div.style.fontSize = "small"
+
+                div.appendChild(document.createElement("span"));//3
+                div.appendChild(document.createElement("span"));//2
+                const label = div.appendChild(document.createElement("label"));//1
+                const subDiv = div.appendChild(document.createElement("div")); // 0
+                div.appendChild(document.createElement("span"));//1
+                div.appendChild(document.createElement("span"));//2
+                div.appendChild(document.createElement("span"));//3
+                div.appendChild(document.createElement("span"));//4
+
+                const subOne = subDiv.appendChild(document.createElement("a"));
+                const span = subDiv.appendChild(document.createElement("span"));
+                const addOne = subDiv.appendChild(document.createElement("a"));
+
+                label.textContent = "闪卡优先级"
+                span.textContent = SPACE + `${priority}` + SPACE;
+
+                addOne.classList.add("b3-button");
+                addOne.classList.add("b3-button--white");
+                addOne.textContent = "➕";
+
+                subOne.classList.add("b3-button");
+                subOne.classList.add("b3-button--white");
+                subOne.textContent = "➖";
+
+                addOne.addEventListener("click", async () => {
+                    await this.updatePrioritySelected([e], priority + 1)
+                    await this.addBtns(element);
+                })
+                subOne.addEventListener("click", async () => {
+                    await this.updatePrioritySelected([e], priority - 1)
+                    await this.addBtns(element);
+                })
+            });
+    }
 }
 
-async function addBtns(element: HTMLElement) {
-    [...element.querySelectorAll(`[${CUSTOM_RIFF_DECKS}]`)]
-        .filter(e => e.querySelectorAll(`[${TOMATO_CONTROL_ELEMENT}]`).length == 0)
-        .map((e: HTMLElement) => {
-            const priority = e.getAttribute(CARD_PRIORITY) ?? "50";
-            const div = e.appendChild(document.createElement("div"));
-            div.setAttribute(TOMATO_CONTROL_ELEMENT, "1");
-            // div.style.paddingLeft = "20px";
-            div.contentEditable = "false";
-            div.style.display = "flex"
-            div.style.justifyContent = "space-between"
-            div.style.fontSize = "small"
-
-            div.appendChild(document.createElement("span"));//2
-            const label = div.appendChild(document.createElement("label"));//1
-            const subDiv = div.appendChild(document.createElement("div")); // 0
-            div.appendChild(document.createElement("span"));//1
-            div.appendChild(document.createElement("span"));//2
-
-            const subOne = subDiv.appendChild(document.createElement("a"));
-            const span = subDiv.appendChild(document.createElement("span"));
-            const addOne = subDiv.appendChild(document.createElement("a"));
-
-            label.textContent = "闪卡优先级"
-            span.textContent = SPACE + `${priority}` + SPACE;
-
-            addOne.classList.add("b3-button");
-            addOne.classList.add("b3-button--white");
-            addOne.textContent = "➕";
-
-            subOne.classList.add("b3-button");
-            subOne.classList.add("b3-button--white");
-            subOne.textContent = "➖";
-        });
-}
 
 function readPriority(ial: AttrType) {
     let priority = Number(ial["custom-card-priority"]);
