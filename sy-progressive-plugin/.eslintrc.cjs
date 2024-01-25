6ede1c0cad3e730c3b2e148b85033fc72734573f
008 <007>
2024-01-25 15:51:53 +0800

    lint

--- sy-progressive-plugin/.eslintrc.cjs
+++ sy-progressive-plugin/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     root: true,
-    env: {node: true, browser: true, es6: true},
+    env: { node: true, browser: true, es6: true },
     parser: "@typescript-eslint/parser",
     plugins: [
         "@typescript-eslint",
@@ -11,16 +11,16 @@ module.exports = {
     ],
     rules: {
         semi: [2, "always"],
-        quotes: [2, "double", {"avoidEscape": true}],
+        quotes: [2, "double", { "avoidEscape": true }],
         "no-async-promise-executor": "off",
         "no-prototype-builtins": "off",
         "no-useless-escape": "off",
         "no-irregular-whitespace": "off",
+        "@typescript-eslint/no-unused-vars": "off",
         "@typescript-eslint/ban-ts-comment": "off",
         "@typescript-eslint/no-var-requires": "off",
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
         "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unused-vars": "off",
     },
 };
