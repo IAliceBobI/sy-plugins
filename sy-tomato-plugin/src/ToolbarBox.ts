140298c7666a7f1ea378f92d4d1f486b4b75a00e
008 <007>
2024-01-28 09:53:47 +0800

    auto

--- sy-tomato-plugin/src/ToolbarBox.ts
+++ sy-tomato-plugin/src/ToolbarBox.ts
@@ -26,6 +26,7 @@ class ToolbarBox {
             callback: async () => {
                 await siyuan.refreshVirtualBlockRef();
                 events.protyleReload();
+                await siyuan.pushMsg("已经刷新虚拟引用");
             }
         });
     }
