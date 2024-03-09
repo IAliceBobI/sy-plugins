b769b77f2f915ffc4a3efecdd3094bba9bd14472
008 <007>
2024-03-10 01:58:12 +0800

    locate

--- sy-tomato-plugin/src/ToolbarBox.ts
+++ sy-tomato-plugin/src/ToolbarBox.ts
@@ -26,11 +26,40 @@ class ToolbarBox {
             position: "left",
             callback: refreshVirRef,
         });
+        this.plugin.addTopBar({
+            icon: "iconFocus",
+            title: this.plugin.i18n.locateDoc,
+            position: "left",
+            callback: locateDoc,
+        });
+
         this.plugin.addCommand({
             langKey: "refreshVirRef",
             hotkey: "",
             callback: refreshVirRef,
         });
+        this.plugin.addCommand({
+            langKey: "locateDoc",
+            hotkey: "⌥1",
+            callback: locateDoc,
+        });
+    }
+}
+
+async function locateDoc() {
+    const docTreeBtn = document.querySelector('[data-title="文档树"][aria-label*="文档树"]') as HTMLButtonElement;
+    if (docTreeBtn) {
+        if (!docTreeBtn.classList.contains("dock__item--active")) {
+            docTreeBtn.click();
+        }
+    }
+    const collapseBtn = document.querySelector('[data-type="collapse"][aria-label*="折叠"]') as HTMLButtonElement;
+    if (collapseBtn) {
+        collapseBtn.click();
+    }
+    const focusBtn = document.querySelector('[data-type="focus"][aria-label*="定位打开的文档"]') as HTMLButtonElement;
+    if (focusBtn) {
+        focusBtn.click();
     }
 }
 
