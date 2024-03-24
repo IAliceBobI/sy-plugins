6466b93232f57f2c18a94250716a78652437f224
008 <007>
2024-03-24 13:58:35 +0800

    like piece digest

--- sy-progressive-plugin/src/digestUtils.ts
+++ sy-progressive-plugin/src/digestUtils.ts
@@ -1,3 +1,4 @@
+import { TEMP_CONTENT } from "../../sy-tomato-plugin/src/libs/gconst";
 import { siyuan } from "../../sy-tomato-plugin/src/libs/utils";
 import { getHPathByDocID } from "./helper";
 
@@ -7,5 +8,6 @@ export async function newDigestDoc(docID: string, boxID: string, idx: string, na
     attr["custom-pdigest-index"] = `${docID}#${idx.padStart(10, "0")}`;
     attr["custom-pdigest-ctime"] = `${docID}#${new Date().getTime()}`;
     attr["custom-off-tomatobacklink"] = "1";
+    attr["custom-progmark"] = `${TEMP_CONTENT}#${docID},999999999`;
     return siyuan.createDocWithMd(boxID, `${hpath}/[${idx}]${name.slice(0, 10)}`, md, "", attr);
 }
